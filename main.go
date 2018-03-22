24b62ba192d314bb1de33bc6f69c8eef88213ad0
Olesii Zubko <oleksii.zubko@outlook.com>
2018-03-22 13:40:32 +0200

    save

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
-func main() {
+import "fmt"
+
+func multipleAssigments() {
+	// you can declare multiple variables at the same time.
+	var x, y, z = 1, 2, 3
+	fmt.Println(x, y, z)
+
+	// they don't even need to be the same type.
+	var a, b, c = 1, "two", 3.3
+	fmt.Println(a, b, c)
+
+	// the short declaration syntax also works.
+	i, j, k := 7, 11, 13
+	fmt.Println(i, j, k)
+
+	// you can use type conversions to ensure the type of
+	// the value is the one you wish.
+	d, e, f := uint16(20), int8(99), uint(12)
+
+	// let's check that d, e, and f are the types we expect
+	var g uint16 = d // would fail if d was not of type uint16
+	var h int8 = e
+	var l uint = f
+
+	fmt.Println(g, h, l)
+}
+
+func loop() {
 	var i = 1
 	for {
 		if i > 10 {
@@ -11,4 +38,31 @@ func main() {
 		}
 		i++
 	}
+}
+
+// A Double returns a number two times larger than i.
+func A(i int) int {
+	return i * 2
+}
+
+// multipleReturnValues switches the values of a and b
+func multipleReturnValues(a int, b int) (int, int) {
+	return b, a
+}
+
+func f() (int, int, int, bool, string) {
+	return 0, 1, 2, true, "test"
+}
+
+func main() {
+	loop()
+	multipleAssigments()
+
+	fmt.Println(A(200))
+
+	a, b := multipleReturnValues(7, 9)
+	fmt.Println(a, b)
+
+	a, _, _, c, _ := f()
+	fmt.Println(a, c)
 }
--- slices/main.go
+++ slices/main.go
@@ -2,13 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-	emptySlice()
-	emptySlice20()
-	zeroValues()
-	nonEmptySlice()
-}
-
 func emptySlice() {
 	var i []int
 	fmt.Println(len(i))
@@ -39,4 +32,57 @@ func nonEmptySlice() {
 	primes[8] = 23
 	primes[9] = 29
 	fmt.Println(primes)
+}
+
+func initializeSlice() {
+	primes := []int{2, 13, 17, 19, 23}
+	fmt.Println(primes)
+}
+
+func appendSlice() {
+	primes := []int{2, 3, 5, 7, 11}
+	fmt.Println(len(primes), primes)
+
+	primes = append(primes, 13)
+	fmt.Println(len(primes), primes)
+
+	primes = append(primes, 17, 19, 23)
+	fmt.Println(len(primes), primes)
+}
+
+func subslice() {
+	brothers := []string{"chico", "harpo", "groucho", "gummo", "zeppo"}
+	fmt.Println(brothers)
+
+	wellknown := brothers[0:3]
+	fmt.Println(wellknown)
+
+	a := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+	fmt.Println(a)
+
+	b := a[0:5]
+	for i := 0; i < len(b); i++ {
+		b[i] = b[i] * -1
+	}
+	fmt.Println(b)
+
+	fmt.Println(a)
+}
+
+func bounds() {
+	// primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
+
+	// fmt.Println(primes[-1])
+	// fmt.Println("bounds: ", primes[10])
+}
+
+func main() {
+	emptySlice()
+	emptySlice20()
+	zeroValues()
+	nonEmptySlice()
+	initializeSlice()
+	appendSlice()
+	subslice()
+	bounds()
 }
