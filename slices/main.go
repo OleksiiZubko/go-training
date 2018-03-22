c8845014fc6c03b5b4302ee139125a202f6e688f
Olesii Zubko <oleksii.zubko@outlook.com>
2018-03-22 13:04:00 +0200

    slices

--- slices/main.go
+++ slices/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	emptySlice()
 	emptySlice20()
+	zeroValues()
+	nonEmptySlice()
 }
 
 func emptySlice() {
@@ -15,4 +17,26 @@ func emptySlice() {
 func emptySlice20() {
 	i := make([]int, 20)
 	fmt.Println(len(i))
+}
+
+func zeroValues() {
+	x := make([]int, 5)
+	for i := 0; i < len(x); i++ {
+		fmt.Println(x[i])
+	}
+}
+
+func nonEmptySlice() {
+	primes := make([]int, 10)
+	primes[0] = 2
+	primes[1] = 3
+	primes[2] = 5
+	primes[3] = 7
+	primes[4] = 11
+	primes[5] = 13
+	primes[6] = 17
+	primes[7] = 19
+	primes[8] = 23
+	primes[9] = 29
+	fmt.Println(primes)
 }
